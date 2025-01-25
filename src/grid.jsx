021549039b115feae4dce5f9d558bac81945282f
onxans <julius@barsekow.de>
2025-01-25 12:18:54 +0100

    Refactor grid layout and styles integration

--- src/grid.jsx
+++ src/grid.jsx
@@ -2,14 +2,16 @@ import GridLayout from "react-grid-layout";
 import { GlobeWindow } from './globe/globeWindow';
 import { LiveUpdatesView } from "./globe/liveupdates";
 import { DetailsView } from "./globe/details";
+import 'react-grid-layout/css/styles.css';
+import 'react-resizable/css/styles.css';
 
 export function Grid() {
     // layout is an array of objects, see the demo for more complete usage
     const layout = [
-      { i: "a", x: 0, y: 0, w: 2, h: 4},
-      { i: "b", x: 2, y: 0, w: 5, h: 5},
-      { i: "c", x: 7, y: 2, w: 1, h: 2 },
-      { i: "d", x: 7, y: 0, w: 1, h: 2 }
+      { i: "a", x: 0, y: 0, w: 2, h: 5},
+      { i: "b", x: 2, y: 0, w: 4, h: 5},
+      { i: "c", x: 7, y: 2, w: 2, h: 2 },
+      { i: "d", x: 7, y: 0, w: 2, h: 3 }
     ];
     return (
       <GridLayout
@@ -18,6 +20,8 @@ export function Grid() {
         cols={8}
         rowHeight={120}
         width={1440}
+        isResizable
+        isDraggable
       >
         <div className="gridItem" key="a"><LiveUpdatesView/></div>
         <div className="gridItem" key="b"><GlobeWindow/></div>
