33472b72511560cdfb21d42b0e6109df64de3012
onxans <julius@barsekow.de>
2025-01-25 10:17:23 +0100

    CesiumTest

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import { Viewer, Entity, PointGraphics, EntityDescription, GeoJsonDataSource } from 'resium'
+import { Cartesian3 } from "cesium";
+
+import './App.css'
+
+// New York Point
+const position = Cartesian3.fromDegrees(-74.0707383, 40.7117244, 100);
+
+// GEOJSON DATA
+const data = {
+  type: "Feature",
+  properties: {
+    name: "Coors Field",
+    amenity: "Baseball Stadium",
+    popupContent: "This is where the Rockies play!",
+  },
+  geometry: {
+    type: "Point",
+    coordinates: [-104.99404, 39.75621],
+  },
+};
+
+function App() {
+  return (
+    <Viewer full>
+      <Entity position={position}>
+        <PointGraphics pixelSize={10}/>
+        <EntityDescription>
+          <h1>New York City</h1>
+          <p>This point marks a location in New York City, known for its iconic skyline and vibrant culture.</p>
+        </EntityDescription>
+        <GeoJsonDataSource data={data} />
+      </Entity> 
+    </Viewer>
+  );
+}
+
+export default App
